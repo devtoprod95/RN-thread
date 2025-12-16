19e9c1bf1439a88ebe5bd88983eb5f91d6c8de74
dohan.kim <junyeob.kim@onch3.co.kr>
2025-12-16 09:25:37 +0900

    feat: expo statusbar 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack } from "expo-router";
 import * as SecureStore from "expo-secure-store";
+import { StatusBar } from "expo-status-bar";
 import { createContext, useEffect, useState } from "react";
 import { Alert } from "react-native";
 
@@ -75,6 +76,7 @@ export default function RootLayout() {
 
   return (
     <AuthContext value={{ user, login, logout, updateUser }}>
+      <StatusBar style="auto" animated />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
