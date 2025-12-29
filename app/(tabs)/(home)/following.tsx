49549c23e420f4a80e6a573fda5d3c5b114ded2e
dohan.kim <junyeob.kim@onch3.co.kr>
2025-12-29 14:49:31 +0900

    feat: 애니메이션 구현2

--- app/(tabs)/(home)/following.tsx
+++ app/(tabs)/(home)/following.tsx
@@ -1,72 +1,133 @@
 import Post, { type Post as PostType } from "@/components/Post";
 import { FlashList } from "@shopify/flash-list";
 import * as Haptics from "expo-haptics";
-import { usePathname, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import {
-    StyleSheet,
-    useColorScheme,
-    View
-} from "react-native";
+import { usePathname } from "expo-router";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import { PanResponder, StyleSheet, useColorScheme, View } from "react-native";
+import Animated, {
+    useAnimatedScrollHandler,
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming,
+} from "react-native-reanimated";
+import { AnimationContext } from "./_layout";
+
+const AnimatedFlashList = Animated.createAnimatedComponent(FlashList<PostType>);
+
 export default function Following() {
-    const router = useRouter();
-    const colorScheme = useColorScheme();
-    const pathname = usePathname();
-    const [posts, setPosts] = useState<PostType[]>([]);
-    const [refreshing, setRefreshing] = useState(false);
+  const colorScheme = useColorScheme();
+  const path = usePathname();
+  const [posts, setPosts] = useState<PostType[]>([]);
+  const scrollPosition = useSharedValue(0);
+  const isReadyToRefresh = useSharedValue(false);
+  const { pullDownPosition } = useContext(AnimationContext);
 
     useEffect(() => {
-        fetch(`/posts?type=following`)
-        .then((res) => res.json())
-        .then((data) => {
-            setPosts(data.posts);
-        });
+        onRefresh(() => {});
     }, []);
 
-    const onEndReached = () => {
-        console.log(`${pathname} onEndReached`);
-        if( posts.length > 0 ){
-            fetch(`/posts?type=following&cursor=${posts.at(-1)?.id}`)
-            .then((res) => res.json())
-            .then((data) => {
-            if( data.posts.length > 0 ){
-                setPosts((prev) => [...prev, ...data.posts]);
-            }
-            })
+  const onEndReached = useCallback(() => {
+    console.log("onEndReached", posts.at(-1)?.id);
+    fetch(`/posts?type=following&cursor=${posts.at(-1)?.id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.posts.length > 0) {
+          setPosts((prev) => [...prev, ...data.posts]);
         }
-    }
+      });
+  }, [posts, path]);
 
-    const onRefresh = () => {
-        setRefreshing(true);
-        setPosts([]);
-        // 진동
-        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        fetch(`/posts?type=following`)
-        .then((res) => res.json())
-        .then((data) => {
-            setPosts(data.posts);
-        })
-        .finally(() => {
-            setRefreshing(false);
-        })
+  const onRefresh = (done: () => void) => {
+    setPosts([]);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    fetch(`/posts?type=following`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data.posts);
+      })
+      .finally(() => {
+        done();
+      });
+  };
+
+  const onPanRelease = () => {
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 60 : 0, {
+      duration: 180,
+    });
+    console.log("onPanRelease", isReadyToRefresh.value);
+    if (isReadyToRefresh.value) {
+      onRefresh(() => {
+        pullDownPosition.value = withTiming(0, {
+          duration: 180,
+        });
+      });
     }
+  };
+
+  const panResponderRef = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        const max = 120;
+        pullDownPosition.value = Math.max(Math.min(gestureState.dy, max), 0);
+        console.log("pull", pullDownPosition.value);
+
+        if (
+          pullDownPosition.value >= max / 2 &&
+          isReadyToRefresh.value === false
+        ) {
+          isReadyToRefresh.value = true;
+        }
+        if (
+          pullDownPosition.value < max / 2 &&
+          isReadyToRefresh.value === true
+        ) {
+          isReadyToRefresh.value = false;
+        }
+      },
+      onPanResponderRelease: onPanRelease,
+      onPanResponderTerminate: onPanRelease,
+    })
+  );
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      console.log("onScroll", event.contentOffset.y);
+      scrollPosition.value = event.contentOffset.y;
+    },
+  });
+
+  const pullDownStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: pullDownPosition.value,
+        },
+      ],
+    };
+  });
 
   return (
-    <View
-        style={[
-            styles.container,
-            colorScheme === "dark" ? styles.containerDark : styles.containerLight,
-        ]}
+    <Animated.View
+      style={[
+        styles.container,
+        colorScheme === "dark" ? styles.containerDark : styles.containerLight,
+        pullDownStyles,
+      ]}
+      {...panResponderRef.current.panHandlers}
     >
-      <FlashList
+      <AnimatedFlashList
+        refreshControl={<View />}
         data={posts}
-        refreshing={refreshing}
-        onRefresh={onRefresh}
+        nestedScrollEnabled={true}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
         renderItem={({ item }) => <Post item={item} />}
         onEndReached={onEndReached}
         onEndReachedThreshold={2}
+        ListEmptyComponent={() => <View style={{ height: 1 }} />}
       />
-    </View>
+    </Animated.View>
   );
 }
 
--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,70 +1,70 @@
 import Post, { type Post as PostType } from "@/components/Post";
 import { FlashList } from "@shopify/flash-list";
 import * as Haptics from "expo-haptics";
-import { usePathname, useRouter } from "expo-router";
-import { useContext, useEffect, useRef, useState } from "react";
-import {
-  PanResponder,
-  StyleSheet,
-  useColorScheme,
-  View
-} from "react-native";
-import Animated, { useAnimatedScrollHandler, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import { usePathname } from "expo-router";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import { PanResponder, StyleSheet, useColorScheme, View } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 import { AnimationContext } from "./_layout";
 
 const AnimatedFlashList = Animated.createAnimatedComponent(FlashList<PostType>);
 
 export default function Index() {
-  const router = useRouter();
   const colorScheme = useColorScheme();
-  const pathname = usePathname();
+  const path = usePathname();
   const [posts, setPosts] = useState<PostType[]>([]);
   const scrollPosition = useSharedValue(0);
   const isReadyToRefresh = useSharedValue(false);
   const { pullDownPosition } = useContext(AnimationContext);
 
+
   useEffect(() => {
-    fetch(`/posts`)
-    .then((res) => res.json())
-    .then((data) => {
-      setPosts(data.posts);
-    });
+    onRefresh(() => {});
   }, []);
 
-  const onEndReached = () => {
-    console.log(`${pathname} onEndReached`);
+  const onEndReached = useCallback(() => {
+    console.log("onEndReached", posts.at(-1)?.id);
     fetch(`/posts?cursor=${posts.at(-1)?.id}`)
-    .then((res) => res.json())
-    .then((data) => {
-      if( data.posts.length > 0 ){
-        setPosts((prev) => [...prev, ...data.posts]);
-      }
-    })
-  };
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.posts.length > 0) {
+          setPosts((prev) => [...prev, ...data.posts]);
+        }
+      });
+  }, [posts, path]);
 
   const onRefresh = (done: () => void) => {
     setPosts([]);
-    // 진동
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-    fetch(`/posts`)
-    .then((res) => res.json())
-    .then((data) => {
-      setPosts(data.posts);
-    })
-    .finally(() => {
-      done();
-    })
-  }
+    fetch("/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data.posts);
+      })
+      .finally(() => {
+        done();
+      });
+  };
 
   const onPanRelease = () => {
-    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 60 : 0, { duration: 180 });
-    if( isReadyToRefresh.value ){
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 60 : 0, {
+      duration: 180,
+    });
+    console.log("onPanRelease", isReadyToRefresh.value);
+    if (isReadyToRefresh.value) {
       onRefresh(() => {
-        pullDownPosition.value = withTiming(0, { duration: 180 });
+        pullDownPosition.value = withTiming(0, {
+          duration: 180,
+        });
       });
     }
-  }
-  
+  };
+
   const panResponderRef = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
@@ -73,30 +73,39 @@ export default function Index() {
         pullDownPosition.value = Math.max(Math.min(gestureState.dy, max), 0);
         console.log("pull", pullDownPosition.value);
 
-        if( pullDownPosition.value >= max / 2 && isReadyToRefresh.value === false ){
+        if (
+          pullDownPosition.value >= max / 2 &&
+          isReadyToRefresh.value === false
+        ) {
           isReadyToRefresh.value = true;
         }
-        if( pullDownPosition.value < max / 2 && isReadyToRefresh.value === true ){
+        if (
+          pullDownPosition.value < max / 2 &&
+          isReadyToRefresh.value === true
+        ) {
           isReadyToRefresh.value = false;
         }
       },
       onPanResponderRelease: onPanRelease,
       onPanResponderTerminate: onPanRelease,
     })
-  )
+  );
 
   const scrollHandler = useAnimatedScrollHandler({
     onScroll: (event) => {
-      'worklet';
-      console.log('onScroll', event.contentOffset.y);
+      console.log("onScroll", event.contentOffset.y);
       scrollPosition.value = event.contentOffset.y;
     },
   });
 
-  const pullDownStyle = useAnimatedStyle(() => {
+  const pullDownStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateY: pullDownPosition.value }]
-    }
+      transform: [
+        {
+          translateY: pullDownPosition.value,
+        },
+      ],
+    };
   });
 
   return (
@@ -104,7 +113,7 @@ export default function Index() {
       style={[
         styles.container,
         colorScheme === "dark" ? styles.containerDark : styles.containerLight,
-        pullDownStyle
+        pullDownStyles,
       ]}
       {...panResponderRef.current.panHandlers}
     >
